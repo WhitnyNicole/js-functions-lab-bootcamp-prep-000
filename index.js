4e9563fa37d6d0deaa67f96748e4d9011897467e
Whitny Edwards <wne4e2@gmail.com>
2018-09-27 00:13:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return (`Happy holidays, ${name}!`) }
 function happyHolidayTo(holiday, name) {
-  return (`Happy ${holidy}, ${name}!`)
+  return (`Happy ${holiday}, ${name}!`)
 }
 function holidayCountdown(holiday, days){
-  return (`It's ${days} days until ${holiday}`) }
+  return (`It's ${days} days until ${holiday}!`) }
